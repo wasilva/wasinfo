326b87106e617600a35d6f5cc74c349e5e5da757
wasilva <william.silva@wasinfo.com.br>
2017-11-29 11:30:50 -0200

    Update noticias 2 (test)

--- noticias.js
+++ noticias.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var  server = http.createServer(function(reg,res){
-	res.end("<html><body>Portal de noticias</body></html>");
+	res.end("<html><body>Portal de noticias 2</body></html>");
 });
 
 server.listen(3000);
